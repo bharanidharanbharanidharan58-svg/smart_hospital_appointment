d725e25aef3bdf7922a324e2864079a062c5fbc8
Bharani dharan <bharanidharanbharanidharan58@gmail.com>
2026-07-27 00:46:58 +0530

    Step 7 api routes

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from routes.auth_routes import auth_bp
 from routes.patient_routes import patient_bp
 from routes.doctor_routes import doctor_bp
 from routes.admin_routes import admin_bp
+from routes.api_routes import api_bp
 
 
 app = Flask(__name__)
@@ -19,11 +20,12 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(patient_bp)
 app.register_blueprint(doctor_bp)
 app.register_blueprint(admin_bp)
+app.register_blueprint(api_bp)
 
 
 @app.route("/")
 def home():
-    return "Step 6 OK"
+    return "Step 7 OK"
 
 
 if __name__ == "__main__":
