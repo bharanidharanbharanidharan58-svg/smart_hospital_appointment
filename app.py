136db0eb36db52ff458c14f0dadf3dc8a6855924
Bharani dharan <bharanidharanbharanidharan58@gmail.com>
2026-07-27 00:29:06 +0530

    Step 2

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-
-app = Flask(__name__)
-
 from config import Config
 
+from models.db import init_db
+from models.hospital import HospitalModel
+from models.doctor import DoctorModel
+
+app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    return "Step 1 OK"
+    return "Step 2 OK"
