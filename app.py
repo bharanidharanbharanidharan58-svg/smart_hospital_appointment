76880a0d37ae55a0c5b6cb3f28bd429b94e2be06
Bharani dharan <bharanidharanbharanidharan58@gmail.com>
2026-07-27 00:37:44 +0530

    Step 4 patient routes

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ from models.hospital import HospitalModel
 from models.doctor import DoctorModel
 
 from routes.auth_routes import auth_bp
+from routes.patient_routes import patient_bp
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
 app.register_blueprint(auth_bp)
+app.register_blueprint(patient_bp)
+
 
 @app.route("/")
 def home():
-    return "Step 3 OK"
+    return "Step 4 OK"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
